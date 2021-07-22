6b409af590b321e815d5f5a8cd7a84ae766909ee
duwenjie <duwenjie@meikeland.com>
2021-07-22 16:19:09 +0800

    GET

--- gee.go
+++ gee.go
@@ -7,17 +7,39 @@ import (
 	"github.com/feiyuanmo/gee/log"
 )
 
-type Engine struct{}
+type HandlerFunc func(w http.ResponseWriter, req *http.Request)
+type Engine struct {
+	router map[string]HandlerFunc
+}
 
-func New() *Engine {
+//func New() *Engine {
+func New() http.Handler {
 	log.Info("------------new gee Engine------------")
-	return &Engine{}
+	return &Engine{router: make(map[string]HandlerFunc)}
+}
+
+func (engine *Engine) addRouter(method, path string, handler HandlerFunc) {
+	key := method + "-" + path
+	engine.router[key] = handler
+}
+
+func (engine *Engine) GET(path string, handler HandlerFunc) {
+	engine.addRouter("GET", path, handler)
 }
 
 func (engine *Engine) ServeHTTP(w http.ResponseWriter, req *http.Request) {
-	fmt.Fprintf(w, "URL.Path = %q\n", req.URL.Path)
+	key := req.Method + "-" + req.URL.Path
+	log.Infof("IP:%s Method:%s Path:%s", req.Host, req.Method, req.URL.Path)
+	hadler, ok := engine.router[key]
+	if ok {
+		hadler(w, req)
+	} else {
+		fmt.Fprintf(w, "404 NOT FOUND: %s\n", req.URL)
+	}
 }
 
+// handler := (http.Handler)(engine)  手动转换为借口类型
+// log.Fatal(http.ListenAndServe(":8080", handler))
 func (engine *Engine) Run(addr string) error {
 	log.Infof("------------Run gee Engine:%s------------", addr)
 	return http.ListenAndServe(addr, engine)
