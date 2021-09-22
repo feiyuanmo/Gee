419d555c6bb835a375c35654c000f26875f93f65
duwenjie <duwenjie@meikeland.com>
2021-09-22 11:32:53 +0800

    fix

--- recovery.go
+++ recovery.go
@@ -3,16 +3,33 @@ package gee
 import (
 	"fmt"
 	"net/http"
+	"runtime"
+	"strings"
 
 	"github.com/feiyuanmo/gee/log"
 )
+
+// print stack trace for debug
+func trace(message string) string {
+	var pcs [32]uintptr
+	n := runtime.Callers(3, pcs[:]) // skip first 3 caller
+
+	var str strings.Builder
+	str.WriteString(message + "\nTraceback:")
+	for _, pc := range pcs[:n] {
+		fn := runtime.FuncForPC(pc)
+		file, line := fn.FileLine(pc)
+		str.WriteString(fmt.Sprintf("\n\t%s:%d", file, line))
+	}
+	return str.String()
+}
 
 func Recovery() HandlerFunc {
 	return func(c *Context) {
 		defer func() {
 			if err := recover(); err != nil {
 				message := fmt.Sprintf("%s", err)
-				log.InfoW("%s\n\n", message)
+				log.InfofW("%s\n\n", trace(message))
 				c.Fail(http.StatusInternalServerError, "Internal Server Error")
 			}
 		}()
